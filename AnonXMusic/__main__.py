5bacb7f44f3e69ac9eccd7ff03e0572b67e4454b
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-19 20:04:12 +0530

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -35,8 +35,12 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("AnonXMusic.plugins" + all_module)
-    LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("VIPMUSIC.plugins").info("Successfully Imported All Modules ")
     await userbot.start()
     await Anony.start()
     try:
